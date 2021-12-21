5ffd5f2b8a0ecb133be1bca72653922d98bce44c
postor <postor@gmail.com>
2021-12-21 17:39:21 +0800

    basic

--- shack.config.mjs
+++ shack.config.mjs
@@ -1,7 +1,6 @@
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
-import ExtraCodeWebpackPlugin from 'extra-code-webpack-plugin'
 
 export default {
   entry: './web/index.tsx',
@@ -26,11 +25,6 @@ export default {
       title: 'my-react',
       template: join(dirname(fileURLToPath(import.meta.url)), 'web', 'index.html'),
     }),
-    new ExtraCodeWebpackPlugin({
-      codes: ({ isDev, isEntry }) => (isDev && isEntry) ? `
-      // @ts-ignore
-      import.meta.webpackHot.accept()` : ``
-    })
   ],
   output: {
     path: join(dirname(fileURLToPath(import.meta.url)), 'dist', 'web'),
